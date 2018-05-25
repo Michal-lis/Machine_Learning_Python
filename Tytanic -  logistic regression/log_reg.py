e3f091d15d2c70e28528639358c9e4d514e27fbd
michal <michallis95@vp.pl>
2018-05-25 15:05:49 +0200

    version1

--- Tytanic -  logistic regression/log_reg.py
+++ Tytanic -  logistic regression/log_reg.py
@@ -153,3 +153,53 @@ X = final_train[Selected_features]
 plt.subplots(figsize=(8, 5))
 sns.heatmap(X.corr(), annot=True, cmap="RdYlGn")
 plt.show()
+
+# MODEL EVALUATION
+from sklearn.model_selection import train_test_split, cross_val_score
+from sklearn.metrics import accuracy_score, classification_report, precision_score, recall_score, confusion_matrix, \
+    precision_recall_curve, roc_curve, auc, log_loss
+
+X = final_train[Selected_features]
+y = final_train['Survived']
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=2)
+# Log reg
+
+logreg = LogisticRegression()
+# trenowanie
+logreg.fit(X_train, y_train)
+# predykcja
+y_pred = logreg.predict(X_test)
+y_pred_proba = logreg.predict_proba(X_test)[:, 1]
+[fpr, tpr, thr] = roc_curve(y_test, y_pred_proba)
+print('Train/Test split results:')
+print(logreg.__class__.__name__ + " accuracy is %2.3f" % accuracy_score(y_test, y_pred))
+print(logreg.__class__.__name__ + " log_loss is %2.3f" % log_loss(y_test, y_pred_proba))
+print(logreg.__class__.__name__ + " auc is %2.3f" % auc(fpr, tpr))
+
+idx = np.min(np.where(tpr > 0.95))  # index of the first threshold for which the sensibility > 0.95
+
+plt.figure()
+plt.plot(fpr, tpr, color='coral', label='ROC curve (area = %0.3f)' % auc(fpr, tpr))
+plt.plot([0, 1], [0, 1], 'k--')
+plt.plot([0, fpr[idx]], [tpr[idx], tpr[idx]], 'k--', color='blue')
+plt.plot([fpr[idx], fpr[idx]], [0, tpr[idx]], 'k--', color='blue')
+plt.xlim([0.0, 1.0])
+plt.ylim([0.0, 1.05])
+plt.xlabel('False Positive Rate (1 - specificity)', fontsize=14)
+plt.ylabel('True Positive Rate (recall)', fontsize=14)
+plt.title('Receiver operating characteristic (ROC) curve')
+plt.legend(loc="lower right")
+plt.show()
+
+print("Using a threshold of %.3f " % thr[idx] + "guarantees a sensitivity of %.3f " % tpr[idx] +
+      "and a specificity of %.3f" % (1 - fpr[idx]) +
+      ", i.e. a false positive rate of %.2f%%." % (np.array(fpr[idx]) * 100))
+
+final_test['Survived'] = log_clf.predict(final_test[Selected_features])
+final_test['PassengerId'] = test_df['PassengerId']
+
+submission = final_test[['PassengerId', 'Survived']]
+
+submission.to_csv("submission.csv", index=False)
+
+submission.tail()
